4e7aa536029b1d62542ef7a439c55a3da56fc642
StinkySteak <slimesteve17@gmail.com>
2024-03-28 11:34:33 +0700

    feat: add more options on TryGet & Has
    several refactor

--- Runtime/ComponentExtensions.cs
+++ Runtime/ComponentExtensions.cs
@@ -4,21 +4,43 @@ namespace StinkySteak.ComponentExtensions
 {
     public static class ComponentExtensions
     {
+        public static bool HasComponent<T>(this Transform transform) where T : Component
+        {
+            T component = transform.GetComponent<T>();
+
+            return component != null;
+        }
+
+        public static bool HasComponent<T>(this Component self, out T component) where T : Component
+         => HasComponent(self.transform, out component);
+        public static bool HasComponent<T>(this GameObject self, out T component) where T : Component
+           => HasComponent(self.transform, out component);
+
         public static bool TryGetComponentInChildren<T>(this Transform transform, out T component) where T : Component
         {
             component = transform.GetComponentInChildren<T>();
 
             return component != null;
         }
-        /// <summary>
-        /// Return error on null parent
-        /// </summary>
+        public static bool TryGetComponentInChildren<T>(this Component self, out T component) where T : Component
+          => TryGetComponentInChildren(self.transform, out component);
+        public static bool TryGetComponentInChildren<T>(this GameObject self, out T component) where T : Component
+           => TryGetComponentInChildren(self.transform, out component);
+
+        #region TRY_GET_COMP_PARENT
+
         public static bool TryGetComponentInParentUnsafe<T>(this Transform transform, out T component) where T : Component
         {
             component = transform.parent.GetComponentInChildren<T>();
 
             return component != null;
         }
+
+        public static bool TryGetComponentInParentUnsafe<T>(this Component self, out T component) where T : Component
+           => TryGetComponentInParentUnsafe(self.transform, out component);
+        public static bool TryGetComponentInParentUnsafe<T>(this GameObject self, out T component) where T : Component
+           => TryGetComponentInParentUnsafe(self.transform, out component);
+
         public static bool TryGetComponentInParent<T>(this Transform transform, out T component) where T : Component
         {
             if (transform.parent == null)
@@ -31,23 +53,19 @@ namespace StinkySteak.ComponentExtensions
 
             return component != null;
         }
-        public static bool HasComponent<T>(this Transform transform) where T : Component
-        {
-            T component = transform.GetComponent<T>();
 
-            return component != null;
-        }
         public static bool TryGetComponentInParent<T>(this Component self, out T component) where T : Component
             => TryGetComponentInParent(self.transform, out component);
-
-        public static bool TryGetComponentInChildren<T>(this Component self, out T component) where T : Component
-           => TryGetComponentInChildren(self.transform, out component);
+        public static bool TryGetComponentInParent<T>(this GameObject self, out T component) where T : Component
+           => TryGetComponentInParent(self.transform, out component);
+        public static bool TryGetComponentInParent<T>(this MonoBehaviour self, out T component) where T : Component
+           => TryGetComponentInParent(self.transform, out component);
 
         /// <summary>
         /// Try find component in this transform, if null, search in parent
         /// </summary>
         /// <returns></returns>
-        public static bool TryGetComponentOrInParent<T>(this Component transform, out T component) where T : Component
+        public static bool TryGetComponentOrInParent<T>(this Transform transform, out T component) where T : Component
         {
             if (transform.TryGetComponent(out T outCompA))
             {
@@ -70,5 +88,11 @@ namespace StinkySteak.ComponentExtensions
             component = null;
             return false;
         }
+        public static bool TryGetComponentOrInParent<T>(this Component self, out T component) where T : Component
+            => TryGetComponentOrInParent(self.transform, out component);
+        public static bool TryGetComponentOrInParent<T>(this GameObject gameObject, out T component) where T : Component
+           => TryGetComponentOrInParent(gameObject.transform, out component);
+        
+        #endregion
     }
 }
